46852fa4bfe383b75f32619dc5e4d569617743c4
Priscila_Canche <priscila.abigail.canche@gmail.com>
2026-02-13 08:02:53 -0600

    nueva modificacion (priscila)

--- PRACTICA 3. CLASES Y METODOS/TermometroDigital.java
+++ PRACTICA 3. CLASES Y METODOS/TermometroDigital.java
@@ -1,1 +1,31 @@
-import java.util.arrayList;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TermometroDigital {
+    private String marca;
+    private boolean encendido;
+    private double temperaturaActual;
+    private String unidad;
+    private ArrayList<Double> historialTemperaturas;
+
+    public TermometroDigital (String marca) {
+        this.marca = marca;
+        this.encendido = false;
+        this.unidad = "C";
+        historialTemperaturas = new ArrayList<>();
+    }
+     public TermometroDigital(){
+        this.marca = "";
+        this.encendido = false;
+        this.unidad = "C";
+        historialTemperaturas = new ArrayList<>();
+     }
+
+     public String getMarca() {return marca;}
+     public void setMarca(String marca){this.marca = marca;}
+    
+     public String getUnidad() {return unidad;}
+     public void setUnidad(String unidad){this.unidad = unidad;}
+     
+    
+}
