9fad2d1ec5a49e03e9e80b5a9cad79325c4226a7
Priscila_Canche <priscila.abigail.canche@gmail.com>
2026-02-19 19:29:10 -0600

    modificacion Priscila

---
+++ PRACTICA 4. HERENCIA/VehiculoLogistico.java
@@ -0,0 +1,54 @@
+public class VehiculoLogistico {
+    
+    protected String placa;
+    protected double capacidadCarga;
+    protected double velocidadPromedio;
+    protected boolean activo;
+
+    public VehiculoLogistico(String placa, double capacidadCarga, double velocidadPromedio){
+        this.placa = placa;
+        this.capacidadCarga = capacidadCarga;
+        this.velocidadPromedio = velocidadPromedio;
+        this.activo = false;
+    }
+    //Getters y setters 
+    public String getPlaca(){
+        return placa;
+    }
+
+    public void setPlaca(String placa){
+        this.placa = placa;
+    }
+
+    public double getCapacidadCarga(){
+        return capacidadCarga;
+    }
+
+    public void setCapacidadCarga(double capacidadCarga){
+        this.capacidadCarga = capacidadCarga;
+    }
+
+    public double getVelocidadPromedio(){
+        return velocidadPromedio;
+    }
+
+    public void setVelocidadPromedio(double velocidadPromedio){
+        this.velocidadPromedio = velocidadPromedio;
+    }
+
+    public void activarVehiculo(){
+        activo = true;
+    }
+
+    public void desactivarVehiculo(){
+        activo = false;
+    }
+
+    public double calcularDiatanciaMaxima(){
+        return velocidadPromedio * 8;
+    }
+
+    public double calcularCostoDelViaje(double distancia){
+        return distancia * 10;
+    }
+}
