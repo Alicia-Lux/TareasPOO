a7d0ae0d3d7173cb7a539072733b926d40ec30ad
Priscila_Canche <priscila.abigail.canche@gmail.com>
2026-03-10 18:37:25 -0600

    Finalizado Priscila Canche

--- PRACTICA 5. HERENCIA_CONSTRUCTORES Y DESTRUCTORES/InstrumentoDeMedicion.java
+++ PRACTICA 5. HERENCIA_CONSTRUCTORES Y DESTRUCTORES/InstrumentoDeMedicion.java
@@ -1,3 +1,38 @@
-public class InstrumentoDeMedicion {
+public abstract class InstrumentoDeMedicion {
+    private String nombre;
+    private String unidadMedida;
+    private double valorActual;
     
+    public InstrumentoDeMedicion(){
+
+    }
+
+    public InstrumentoDeMedicion(String nombre, String unidadMedida, double valorActual){
+    }
+
+    public String getnombre(){
+        return nombre;
+    }
+    public void setnombre(String nombre){
+        if (nombre != null && !nombre.isEmpty()) {
+            this.nombre = nombre;
+        }
+    }
+
+    public String getunidadMedida(){
+        return unidadMedida;
+    }
+    public void setunidadMedida(String unidadMedida){
+        this.unidadMedida = unidadMedida;
+    }
+
+    public double getvalorActual(){
+        return valorActual;
+    }
+    public void setvalorActual(double valorActual){
+        if (valorActual >= 0) {
+            this.valorActual = valorActual;
+        }
+    }
+    public abstract void medir();
 }
