6467a988a8c56c02cd91065b3b55a86a68c43bb7
Priscila_Canche <priscila.abigail.canche@gmail.com>
2026-03-10 18:13:39 -0600

    Comienzo Priscila Canche

---
+++ PRACTICA 5. HERENCIA_CONSTRUCTORES Y DESTRUCTORES/Termometro.java
@@ -0,0 +1,52 @@
+public class Termometro {
+        //Atributos
+    private String marca;
+    private String color;
+    private double valor;      
+    private boolean botonOnOff;
+
+    //Constructor
+    public Termometro(String marca) {
+        this.marca = marca;
+        this.color = "Blanco";     
+        this.valor = 0.0;
+        this.botonOnOff = false;   
+    }
+
+    //Métodos GET y SET
+    // Marca
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    // Color
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    // Valor (temperatura)
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    // Botón On/Off
+    public void setBotonOnOff(boolean botonOnOff) {
+        this.botonOnOff = botonOnOff;
+    }
+
+    public boolean getBotonOnOff() {
+        return botonOnOff;
+    }
+}
