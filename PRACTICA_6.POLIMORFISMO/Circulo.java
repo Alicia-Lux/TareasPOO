e461aa1a507cebc693d86ac2d726791e0ca77c55
LEANDRO <10235@itescam.edu.mx>
2026-05-16 03:24:20 -0600

    Refactor Circulo class by removing duplicate code

--- PRACTICA_6.POLIMORFISMO/Circulo.java
+++ PRACTICA_6.POLIMORFISMO/Circulo.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 public class Circulo extends FiguraGeometrica {
     
     private double radio;
@@ -24,30 +23,3 @@ public class Circulo extends FiguraGeometrica {
     }
     
 }
-=======
-public class Circulo extends FiguraGeometrica {
-    
-    private double radio;
-
-    public Circulo(double radio) {
-        this.radio = radio;
-    }
-
-    @Override
-    public void dibujar() {
-        //Código para dibujar un circulo
-        System.out.println("Dibujando un círculo");
-    }
-    
-    public double calcularArea() {
-        //Codigo para calcular el área de un circulo
-        return Math.PI * radio * radio;
-    }
-    
-    public double calcularPerimetro() {
-        //Código para calcular el perímetro de un circulo
-        return 2 * radio * Math.PI;
-    }
-    
-}
->>>>>>> e8d6b5a742efe8c51ba0d1220b377c654818c95b
